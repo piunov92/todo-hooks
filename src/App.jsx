f62aef4a74a461dba0af45d2c0151baf5fd11ed9
Sergey Piunov <piunov92@gmail.com>
2024-04-02 21:59:50 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import TaskInput from './components/NewTaskForm/NewTaskForm'
 import TaskList from './components/TaskList/TaskList'
 import Footer from './components/Footer/Footer'
-import TaskFilter from './components/TasksFilter/TaskFilter'
+import TaskFilter from './components/TasksFilter/TaskFilter';
 
 function App() {
   const [data, setData] = React.useState([])
