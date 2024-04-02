6b2476b0d41d5fdd581723782f91bd73ff2f81ae
Sergey Piunov <piunov92@gmail.com>
2024-04-02 22:06:52 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import TaskInput from './components/NewTaskForm/NewTaskForm'
-import TaskList from './components/TaskList/TaskList'
+import TaskList from './components/TaskList/TaskList';
 import Footer from './components/Footer/Footer';
 import TaskFilter from './components/TasksFilter/TaskFilter';
 
